c7d9b92aba6f6454595282aec7eaecba2df35874
Claude20022002 <clusamote@gmail.com>
2025-07-23 17:28:09 +0100

    fix: correction du chemin d'importation des routes utilisateur et réintroduction du middleware d'authentification

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 // Import routes
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoute");
 
 const app = express();
 app.use(cors());
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userControl");
-const auth = require("../Middlewares/auth");
+const auth = require("../middlewares/auth");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
